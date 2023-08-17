eca80668d232cd0c830b0551ba3c4f5619b9647c
azoth zephyr <0x@zephyr.sx>
2023-08-17 13:22:22 -0700

    dockerize all the things, clean up generate.sh script, modify readme

--- sdk/README.md
+++ sdk/README.md
@@ -1,30 +1,9 @@
 ## Protobuf SDKS
 
-In this directory you will find SDKs for a variety of languages, generated from protobufs using protoc. You can find the protos and the script used to generate each of the individual languages files in the protos/ directory.  
+In this directory you will find SDKs for a variety of languages, generated from protobufs using protoc. You can find the protos and the script used to generate each of the individual languages files in the `protos/`` directory.  
 
-####
-Build Requirements:
-```
-# protoc install for apt based systems
-apt install -y protobuf-compiler
-
-# on non-apt based linux, see: https://github.com/google/protobuf/releases and ensure proper protoc build for your environment
-# PB_REL="https://github.com/protocolbuffers/protobuf/releases"
-# curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
-# unzip protoc-3.15.8-linux-x86_64.zip -d $HOME/.local
-# export PATH="$PATH:$HOME/.local/bin"
-
-# on mac:
-# brew install protobuf
-
-# language specific tools:
-
-# for typescript library generation
-npm install -g protoc-gen-ts
-
-# for golang generation, ensure $GOPATH is in your path
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-```
+#### Build Requirements:
+To build new libraries from the protos in protos/, install docker and run generate.sh script
 
 Individual Documentation on protobufs:
 - [cpp](https://protobuf.dev/getting-started/cpptutorial/)
@@ -38,4 +17,3 @@ Individual Documentation on protobufs:
 # TODO:
 - golang build
 - csharp build
-- Create build docker container 
--- sdk/generate.sh
+++ sdk/generate.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+# 
+# This script build the protobuf generation image in Dockerfile,
+# creates a container using the image, copies the various sdks
+# to their respective files, deletes the container / image,
+# and exits.
+#
+# only requirement is docker
 
-protoc --python_out=../python/ *.proto
-protoc --cpp_out=../cpp/ *.proto
-protoc --java_out=../java/ *.proto
+# build container and generate sdks on build
+docker build -f Dockerfile . -t proto-gen:latest
 
-# generating CommonJS style import, if Closer-style imports are needed
-# see: https://github.com/protocolbuffers/protobuf-javascript
-protoc --js_out=import_style=commonjs,binary:../javascript *.proto
+# copy files from container to host sdk directory
+docker create --name proto-copy proto-gen:latest
+docker cp proto-copy:/home/proto/cpp/ ./
+docker cp proto-copy:/home/proto/java/ ./
+docker cp proto-copy:/home/proto/javascript/ ./
+docker cp proto-copy:/home/proto/python/ ./
+docker cp proto-copy:/home/proto/typescript/ ./
+# docker cp proto-copy:/home/proto/go/ go/
+# docker cp proto-copy:/home/proto/csharp/ csharp/
 
-# requires protoc-gen-ts, see https://github.com/thesayyn/protoc-gen-ts
-protoc --ts_out=../typescript/ *.proto
-
-
-
-# Golang requires an option to be added to proto
-# option go_package in the protos
-# see: https://protobuf.dev/getting-started/gotutorial/
-# protoc -M --go_out=../go/ ./*.proto
-
-
-# TODO: for c sharp, we need to specify a namespace in proto
-# and make ../csharp dir
-# protoc --csharp_out=../csharp/ *.proto
+docker rm -f proto-copy 
+docker image rm proto-gen:latest
+clear
+echo 'language specific imports created from proto files in protos/ directory'
